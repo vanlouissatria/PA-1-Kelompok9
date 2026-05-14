3a52d85565bef98af4d0c819b265756b7167d08b
ivanajahhkali-byte <ivanajahhkali@gmail.com>
2026-05-14 20:04:20 +0700

    commit

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin GeoToba',
+            'email' => 'teleefratasihotanggeosite@gmail.com',
+            'password' => Hash::make('password123'), // Ganti dengan password yang Anda inginkan
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
