ecc327c9c5c4323f97d009ac38d226c6b9343944
amentasitinjak <amentasitinjak04@gmail.com>
2026-06-02 14:23:15 +0700

    salah nama, harusnya class sesuai nama file

--- app/Http/Controllers/Admin/adminFasilitasController.php
+++ app/Http/Controllers/Admin/adminFasilitasController.php
@@ -7,7 +7,7 @@ use App\Models\Fasilitas;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
-class FasilitasController extends Controller
+class adminFasilitasController extends Controller
 {
     public function index()
     {
